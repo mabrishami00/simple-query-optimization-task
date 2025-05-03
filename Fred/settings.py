fb09ea69016aeaf1c33516432559ccdc5e1b43d6
rashvand <mohammadhoseinrashvand@gmail.com>
2025-05-03 18:33:31 +0330

    Add User model

--- Fred/settings.py
+++ Fred/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app.apps.MainAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'main_app.FredUser'
+
---
+++ main_app/models/__init__.py
@@ -0,0 +1,1 @@
+from .fred_user import FredUser
